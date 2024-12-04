6eac3db7c0e366483e76b48de19ed9259ac47214
EmmanuelRG <88853327+EmmanuelRGuesser@users.noreply.github.com>
2024-12-04 16:14:27 -0300

    Ajuste nas mensagens mqtt

--- main/main.c
+++ main/main.c
@@ -5,6 +5,8 @@
 #include "freertos/task.h"
 #include "mqtt_client.h"
 
+#include "esp_log.h"
+#include "driver/i2c.h"
 #include "pzem004tv3.h"
 #include "wifi_connect.h"
 
@@ -26,7 +28,7 @@ void PMonTask( void * pz );
 void app_main()
 {
     // Configurar o GPIOs
-    gpio_set_direction(GPIO_NUM_6, GPIO_MODE_OUTPUT); // Rele
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT); // Rele
     gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT); // led wifi
 
     nvs_flash_init();
@@ -44,24 +46,24 @@ void PMonTask( void * pz )
             printf( "Vrms: %.1fV - Irms: %.3fA - P: %.1fW - E: %.2fWh\n", pzValues.voltage, pzValues.current, pzValues.power, pzValues.energy );
             printf( "Freq: %.1fHz - PF: %.2f\n", pzValues.frequency, pzValues.pf );
             
-            char message[10];
-            snprintf(message, sizeof(message), "{%.2f}", pzValues.voltage);
+            char message[20];
+            snprintf(message, sizeof(message), "%.2f", pzValues.voltage);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_VOLTAGE, message, 0, 1, 0);
-            snprintf(message, sizeof(message), "{%.2f}", pzValues.current);
+            snprintf(message, sizeof(message), "%.2f", pzValues.current);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_CURRENT, message, 0, 1, 0);
-            snprintf(message, sizeof(message), "{%.2f}", pzValues.power);
+            snprintf(message, sizeof(message), "%.2f", pzValues.power);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_POWER, message, 0, 1, 0);
-            snprintf(message, sizeof(message), "{%.2f}", pzValues.energy);
+            snprintf(message, sizeof(message), "%.2f", pzValues.energy);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_ENERGY, message, 0, 1, 0);
-            snprintf(message, sizeof(message), "{%.2f}", pzValues.frequency);
+            snprintf(message, sizeof(message), "%.2f", pzValues.frequency);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_FREQUENCY, message, 0, 1, 0);
-            snprintf(message, sizeof(message), "{%.2f}", pzValues.pf);
+            snprintf(message, sizeof(message), "%.2f", pzValues.pf);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_PF, message, 0, 1, 0);
         }
 
         ESP_LOGI( TAG, "Stack High Water Mark: %ld Bytes free", ( unsigned long int ) uxTaskGetStackHighWaterMark( NULL ) );     /* Show's what's left of the specified stacksize */
 
-        vTaskDelay(pdMS_TO_TICKS(2500));
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
     vTaskDelete( NULL );
 }
