c8bfd7ae6639cbd2cda66431060eff4e02f247ca
EmmanuelRG <88853327+EmmanuelRGuesser@users.noreply.github.com>
2025-01-14 10:54:19 -0300

    Add LCD Monitor

--- main/config.h
+++ main/config.h
@@ -1,15 +1,15 @@
 // Configurações de Wi-Fi
-#define WIFI_SSID "USER_SSID"
-#define WIFI_PASS "USER_PASSWORD"
+#define WIFI_SSID "GUESSER"
+#define WIFI_PASS "guesser123"
 
 // Configurações MQTT
-#define MQTT_BROKER_URI     "mqtt://192.168.3.127"
-#define MQTT_TOPIC_VOLTAGE  "homeassistant/sensor/voltage"
-#define MQTT_TOPIC_CURRENT  "homeassistant/sensor/current"
-#define MQTT_TOPIC_POWER    "homeassistant/sensor/power"
-#define MQTT_TOPIC_ENERGY   "homeassistant/sensor/energy"
-#define MQTT_TOPIC_FREQUENCY "homeassistant/sensor/frequency"
-#define MQTT_TOPIC_PF       "homeassistant/sensor/pf"
+#define MQTT_BROKER_URI      "mqtt://192.168.3.25"
+#define MQTT_TOPIC_VOLTAGE   "sensor/voltage"
+#define MQTT_TOPIC_CURRENT   "sensor/current"
+#define MQTT_TOPIC_POWER     "sensor/power"
+#define MQTT_TOPIC_ENERGY    "sensor/energy"
+#define MQTT_TOPIC_FREQUENCY "sensor/frequency"
+#define MQTT_TOPIC_PF        "sensor/pf"
 
 // Configurações do home assistant
 #define username_ha "projetopi"
--- main/main.c
+++ main/main.c
@@ -5,9 +5,9 @@
 #include "freertos/task.h"
 #include "mqtt_client.h"
 
-#include "esp_log.h"
-#include "driver/i2c.h"
 #include "pzem004tv3.h"
+#include "hd44780.h"
+#include "pcf8574.h"
 #include "wifi_connect.h"
 
 /* @brief Set ESP32  Serial Configuration */
@@ -21,32 +21,43 @@ pzem_setup_t pzConf =
 
 static const char * TAG = "APP_MAIN";
 TaskHandle_t PMonTHandle = NULL;
+TaskHandle_t LcdHandle = NULL;
 _current_values_t pzValues;            /* Measured values */
 
-void PMonTask( void * pz );
+static i2c_dev_t pcf8574;
+
+static esp_err_t write_lcd_data(const hd44780_t *lcd, uint8_t data)
+{
+    return pcf8574_port_write(&pcf8574, data);
+}
+
+void PMonTask( void * pvParameters );
+void lcdTask( void * pvParameters );
 
 void app_main()
 {
     // Configurar o GPIOs
-    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT); // Rele
+    gpio_set_direction(GPIO_NUM_6, GPIO_MODE_OUTPUT); // Rele
     gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT); // led wifi
 
     nvs_flash_init();
     wifi_init();
 
     xTaskCreate( PMonTask, "PowerMon", ( 512 * 8 ), NULL, tskIDLE_PRIORITY, &PMonTHandle );
+    ESP_ERROR_CHECK(i2cdev_init());
+    xTaskCreate( lcdTask, "LCD", (512 * 8), NULL, tskIDLE_PRIORITY + 1, &LcdHandle );
 }
 
-void PMonTask( void * pz )
+void PMonTask( void * pvParameters )
 {
     PzemInit( &pzConf );
     while(1)
     {        
-        if (mqtt_client != NULL && PzemGetValues( &pzConf, &pzValues ) == true) {
+        if (mqtt_client != NULL) {
             printf( "Vrms: %.1fV - Irms: %.3fA - P: %.1fW - E: %.2fWh\n", pzValues.voltage, pzValues.current, pzValues.power, pzValues.energy );
             printf( "Freq: %.1fHz - PF: %.2f\n", pzValues.frequency, pzValues.pf );
             
-            char message[20];
+            char message[10];
             snprintf(message, sizeof(message), "%.2f", pzValues.voltage);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_VOLTAGE, message, 0, 1, 0);
             snprintf(message, sizeof(message), "%.2f", pzValues.current);
@@ -60,10 +71,65 @@ void PMonTask( void * pz )
             snprintf(message, sizeof(message), "%.2f", pzValues.pf);
             esp_mqtt_client_publish(mqtt_client, MQTT_TOPIC_PF, message, 0, 1, 0);
         }
-
-        ESP_LOGI( TAG, "Stack High Water Mark: %ld Bytes free", ( unsigned long int ) uxTaskGetStackHighWaterMark( NULL ) );     /* Show's what's left of the specified stacksize */
-
+        //ESP_LOGI( TAG, "Stack High Water Mark: %ld Bytes free", ( unsigned long int ) uxTaskGetStackHighWaterMark( NULL ) );     /* Show's what's left of the specified stacksize */
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
+    vTaskDelete( NULL );
+}
+
+void lcdTask(void *pvParameters)
+{
+    hd44780_t lcd = {
+        .write_cb = write_lcd_data, // use callback to send data to LCD by I2C GPIO expander
+        .font = HD44780_FONT_5X8,
+        .lines = 4,
+        .pins = {
+            .rs = 0,
+            .e  = 2,
+            .d4 = 4,
+            .d5 = 5,
+            .d6 = 6,
+            .d7 = 7,
+            .bl = 3
+        }
+    };
+
+    memset(&pcf8574, 0, sizeof(i2c_dev_t));
+    ESP_ERROR_CHECK(pcf8574_init_desc(&pcf8574, 0x3f, 0, 36, 37));
+    ESP_ERROR_CHECK(hd44780_init(&lcd));
+    hd44780_switch_backlight(&lcd, true);
+
+    hd44780_gotoxy(&lcd, 0, 0);
+    hd44780_puts(&lcd, "Projeto de PIII");
+    hd44780_gotoxy(&lcd, 0, 2);
+    hd44780_puts(&lcd, "Medidor de Energia");
+    vTaskDelay(pdMS_TO_TICKS(3000));
+    hd44780_clear(&lcd);
+
+    while(1){
+        if(PzemGetValues( &pzConf, &pzValues ))
+        {
+            char message[24];
+
+            hd44780_gotoxy(&lcd, 0, 0);
+            snprintf(message, sizeof(message), "Voltage: %.2f V", pzValues.voltage);
+            hd44780_puts(&lcd, message);
+
+            hd44780_gotoxy(&lcd, 0, 1);
+            snprintf(message, sizeof(message), "Current: %.2f A", pzValues.current);
+            hd44780_puts(&lcd, message);
+
+            hd44780_gotoxy(&lcd, 0, 2);
+            snprintf(message, sizeof(message), "Power: %.2f W", pzValues.power);
+            hd44780_puts(&lcd, message);
+
+            hd44780_gotoxy(&lcd, 0, 3);
+            snprintf(message, sizeof(message), "Energy: %.2f Wh", pzValues.energy);
+            hd44780_puts(&lcd, message);
+
+            //ESP_LOGI( TAG, "Stack High Water Mark: %ld Bytes free", ( unsigned long int ) uxTaskGetStackHighWaterMark( NULL ) );
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    };
     vTaskDelete( NULL );
 }
